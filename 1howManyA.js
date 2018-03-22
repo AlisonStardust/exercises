411ced794bd4b9a8b06b75ca5b4db2924955e070
alisonstardust <zakulisami@o2.pl>
2018-03-22 18:32:32 +0100

    clean up

--- 1howManyA.js
+++ 1howManyA.js
@@ -1,14 +1,3 @@
-// function howManyA(str) {
-//   var empty = [];
-//   var words = str.split(" ");
-//   var wordsWithA = words.forEach(function(word) {
-//     if (word[0] == 'A') {
-//       empty.push(word);
-//     };
-//   });
-//   return empty;
-// }
-
 function arrayEquals(a1, a2) {
   return a1.length == a2.length && a1.every((val, ind) => val === a2[ind]);
 }
@@ -25,5 +14,3 @@ console.assert(arrayEquals(allWordsWithA("Ala"), ["Ala"]));
 console.assert(arrayEquals(allWordsWithA("Ala Adam"), ["Ala", "Adam"]));
 console.assert(arrayEquals(allWordsWithA("Ala kot Adam"), ["Ala", "Adam"]));
 console.assert(arrayEquals(allWordsWithA("Ala ma arbuza"), ["Ala", "arbuza"]));
-
-//console.log(howManyA("Ala ma Ala kota"));
--- christmasTree.js
+++ christmasTree.js
@@ -16,16 +16,3 @@ function row(height) {
 }
 
 row(10);
-
-//n == 0 -> g == 1
-//n == 1 -> g == 3
-//n == 2 -> g == 5
-//n == 3 -> g == 7
-//n == 4 -> g == 9
-//gn(i) == 
-
-//i == height-2 -> 2
-//i == height-3 -> 3
-
-//s(i) == height-i
-
